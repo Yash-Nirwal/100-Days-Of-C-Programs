2bb9016d59eabfeff66cc2f520c8478a61e1a444
Yash Nirwal <yashnirwal@outlook.com>
2025-09-13 14:32:04 +0530

    Update and rename code1.c to Test.c
    This is a test code to check git link is working or not

---
+++ Test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Hi this is my first test program on git hub to check git link is working//
+    printf("I like Pizza");
+return 0;
+}
+
--- code1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Hi this is my first program //
-    printf("I like Pizza");
-return 0;
-}
