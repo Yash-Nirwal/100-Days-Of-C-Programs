84f79d9568dd2af8a785f35fd3f5457b4e7bb236
Yash Nirwal <yashnirwal@outlook.com>
2025-10-30 21:03:32 +0530

    Update Day22_B_Sum-Of-Series.c

--- DAY 21 to 30 code/Day22_B_Sum-Of-Series.c
+++ DAY 21 to 30 code/Day22_B_Sum-Of-Series.c
@@ -1,4 +1,4 @@
-///Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
 #include <stdio.h>
 int main() {
     int n;
@@ -12,3 +12,4 @@ int main() {
     printf("Approximate sum: %.1f\n", sum);
     return 0;
 }
+
